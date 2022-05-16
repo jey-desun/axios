05a8779018a7b883720d1a5ed84b128fb32ad134
taguchi <j.taguchi@sonicmoov.com>
2022-05-16 12:12:23 +0900

    [modified] Lambda+TypeScript+AxiosでAPIを叩いてみよう

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,18 @@
+import {Axios} from "axios";
+
 module.exports.handler = async (event: any, context: any, callback: any) => {
-  console.log("event = ", event)
+  const axios = new Axios({
+    baseURL: 'https://api.open-meteo.com/v1/'
+  });
+
+  const result = await axios.get('forecast', {
+    params: {
+      latitude: 52.52,
+      longitude: 13.41,
+      hourly: 'temperature_2m'
+    }
+  });
+
+  console.log("result = ", result);
   callback(null, 'DONE');
 }
